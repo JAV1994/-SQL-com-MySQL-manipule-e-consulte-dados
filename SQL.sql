e88e19e3c4b209b4a50bf5be43bbf2c524f8d243
JAV1994 <jose.valim@escola.pr.gov.br>
2025-10-16 09:42:41 -0300

    feat: criacao tabela cliente

---
+++ SQL.sql
@@ -0,0 +1,14 @@
+Create Table tbCliente
+(CPF varchar(11),
+NOME varchar(100),
+ENDERECO1 varchar(150),
+ENDERECO2 varchar(150),
+BAIRRO varchar(50),
+CIDADE varchar(50),
+ESTADO varchar(50),
+CEP varchar(50),
+IDADE smallint,
+SEXO varchar(1),
+LIMITE_CREDITO float,
+VOLUME_COMPRA float,
+PRIMEIRA_COMPRA BIT(1));
